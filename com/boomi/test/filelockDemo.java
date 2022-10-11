cea7a3048eba9827fde842b95ad2cf33be24dbfe
Richard Rudd <richard@W10-3SRPVD3.localdomain>
2022-10-11 13:30:41 +0100

    forth commit

--- com/boomi/test/filelockDemo.java
+++ com/boomi/test/filelockDemo.java
@@ -13,9 +13,9 @@ import java.text.MessageFormat;
 public class filelockDemo {
 	public static final int MAX_LOCKS = 100;
 	public static final String FMT_FAILED = "Failed locking file {0}";
-	public static final String FMT_LOCK = "{0}: Locked file {1}";
-	public static final String FMT_UNLOCK = "{0}: Unlocked file {1}";
-	public static final String FMT_WRITE = "{0}: Written to file {1}";
+	public static final String FMT_LOCK = "{0}: Locked file {1} in {2} ms";
+	public static final String FMT_UNLOCK = "{0}: Unlocked file {1} in {2} ms";
+	public static final String FMT_WRITE = "{0}: Written to file {1} in {2} ms";
 
 	public static void main(String[] args) {
 		RandomAccessFile file = null;
@@ -32,6 +32,11 @@ public class filelockDemo {
 
 			int tryLocks = 1;
 			while (tryLocks <= MAX_LOCKS) {
+				
+				// randomize wait timeout
+				int timeout = new Random().nextInt(1000);				
+				
+				long startLock = System.currentTimeMillis();
 				try {
 					// Attempt to acquire an exclusive lock
 					file = new RandomAccessFile(lockFile, "rw");
@@ -41,19 +46,19 @@ public class filelockDemo {
 					e.printStackTrace();
 				}				
 
-				int timeout = new Random().nextInt(1000);
-
 				if (fileLock == null) {
 					System.out.println(MessageFormat.format(FMT_FAILED, lockFile.getAbsolutePath()));
 					Thread.sleep(timeout);//1
 				} else {
-					System.out.println(MessageFormat.format(FMT_LOCK, tryLocks, lockFile.getAbsolutePath()));
+					System.out.println(MessageFormat.format(FMT_LOCK, tryLocks, lockFile.getAbsolutePath(), System.currentTimeMillis()-startLock));
 					if (file != null) {
+						long writeTime = System.currentTimeMillis();
 						file.writeChars("writing after lock");
-						System.out.println(MessageFormat.format(FMT_WRITE, tryLocks, lockFile.getAbsolutePath()));
+						System.out.println(MessageFormat.format(FMT_WRITE, tryLocks, lockFile.getAbsolutePath(), System.currentTimeMillis()-writeTime));
 					}
+					long releaseLock = System.currentTimeMillis();
 					releaseLockChannel(channel);
-					System.out.println(MessageFormat.format(FMT_UNLOCK, tryLocks, lockFile.getAbsolutePath()));
+					System.out.println(MessageFormat.format(FMT_UNLOCK, tryLocks, lockFile.getAbsolutePath(), System.currentTimeMillis()-releaseLock));
 					Thread.sleep(timeout);//1
 				}
 				tryLocks++;
